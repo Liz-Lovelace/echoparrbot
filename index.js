813bb89022900677397e6b82f5ea4f7284f1c71d
Mishanya644 <ivanov0604041@gmail.com>
2021-06-15 18:31:35 +0300

    put token into separate file lol

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 "use strict"
+const fs = require('fs');
 const { Telegraf } = require('telegraf');
-const bot = new Telegraf('1762095434:AAEjeQLajwUbzZnAdC2geN2AlEvg8RF3Us8');
+
+function readTokenFromFile(filePath){
+  try{
+    return fs.readFileSync(filePath, 'utf8');
+  } catch(err) {
+    console.log('Error while reading token.txt');
+    console.log(err);
+    return null;
+  }
+}
+
+let token = readTokenFromFile('./token.txt');
+const bot = new Telegraf(token);
+
 bot.command('start', ctx=>{
   console.log(ctx.from);
   bot.telegram.sendMessage(ctx.chat.id, 'I\'m straight up out here');
